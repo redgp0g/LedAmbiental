86dbec6e51748bbb33ee9d717639c3df824da82a
Guilherme Gordiano <guilhermeg2004@gmail.com>
2023-08-08 20:59:56 -0300

    Função para criar e deletar novos campos de material implementada

--- Program.cs
+++ Program.cs
@@ -43,6 +43,6 @@ app.UseAuthorization();
 app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}");
 
 app.Run();
